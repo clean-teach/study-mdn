8d384e8d12aa45f7ac126d450f2621a92288cc48
kch <kch19920726@gmail.com>
2022-02-10 17:54:54 +0900

    MDN 학습 내용 백업
    - 진행상황은 노션 참고

---
+++ javascript/Client-side-web-APIs/Client-side-storage/index-db/index.js
@@ -0,0 +1,15 @@
+// Create needed constants
+const list = document.querySelector('ul');
+const titleInput = document.querySelector('#title');
+const bodyInput = document.querySelector('#body');
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('form button');
+
+// Create an instance of a db object for us to store the open database in
+let db;
+
+window.onload = function() {
+    // Open our database; it is created if it doesn't already exist
+    // (see onupgradeneeded below)
+    let request = indexedDB.open('notes_db', 1);
+};
